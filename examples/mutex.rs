a143e763a1de5f6bf547de815aefcdec55434bb1
Rob Gries <robgrie@gmail.com>
2019-11-20 16:07:18 -0500

    Run cargo fmt

--- examples/mutex.rs
+++ examples/mutex.rs
@@ -4,9 +4,9 @@ fn main() {
     let locked_data: Mutex<u32> = Mutex::new(42);
     println!("{:?}", locked_data); // Mutex { data: 42 }
     {
-            let mut data: MutexGuard<u32>  = locked_data.lock().unwrap();
-                *data = 24;
-                    println!("{:?}", locked_data); // Mutex { data: <locked> }
+        let mut data: MutexGuard<u32> = locked_data.lock().unwrap();
+        *data = 24;
+        println!("{:?}", locked_data); // Mutex { data: <locked> }
     }
     println!("{:?}", locked_data); // Mutex { data: 24 }
 }
--- examples/tcp_listener.rs
+++ examples/tcp_listener.rs
@@ -9,4 +9,3 @@ fn main() {
         println!("Connection established!");
     }
 }
-
