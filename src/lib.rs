fcd7ae9b99120dfe5b874d2b9bce63dc8a1e547e
Rob Gries <robert.w.gries@gmail.com>
2019-11-25 17:52:24 -0500

    Add print message

--- src/lib.rs
+++ src/lib.rs
@@ -81,6 +81,7 @@ impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || {
             loop {
+                println!("Worker {} got a job; executing.", id);
                 let job: Job;
                 // TODO: Read jobs from the channel and store in variable
                 // TODO: Uncomment me!
