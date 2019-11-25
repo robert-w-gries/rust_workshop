01f83c50c93ba0c09a8a2e6d3e8a56de19f16e1d
Rob Gries <robert.w.gries@gmail.com>
2019-11-25 17:46:52 -0500

    Add more comments and TODOs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,19 @@
 #![allow(dead_code)]
-
-use std::sync::{Arc, Mutex};
-use std::sync::mpsc::{self, Receiver};
+use std::sync::{Arc, Mutex, mpsc};
 use std::thread;
 
 #[derive(Debug)]
+/// Errors that can occur during thread pool creation
 pub enum PoolCreationError {
+    /// Zero-sized thread pools are not valid
     EmptyPool,
+    // Homework: what other errors could be added here?
 }
 
+/// A pool of threads that can asynchronously execute jobs
+///
+/// Once a `ThreadPool` is created, it will generate a given number of threads,
+/// which can asynchronously execute arbitrary jobs passed via [`execute`].
 pub struct ThreadPool {
     workers: Vec<Worker>,
     sender: mpsc::Sender<Job>,
@@ -17,17 +22,30 @@ pub struct ThreadPool {
 type Job = Box<dyn FnBox + Send + 'static>;
 
 impl ThreadPool {
+    /// Create a pool of threads with the specified size.
+    ///
+    /// Note: A pool size of zero will result in an error
     pub fn new(size: usize) -> Result<ThreadPool, PoolCreationError> {
         if size == 0 {
             return Err(PoolCreationError::EmptyPool)
         }
 
+        #[allow(unused_mut)]
         let mut workers = Vec::with_capacity(size);
 
-        // TODO: Create reference counted mutex for receiver
+        // Create a channel that will live as long as the `ThreadPool` lives
+        // `ThreadPool` will own the `Sender` and use it to send jobs to the
+        // next available worker
         let (sender, receiver) = mpsc::channel();
 
+        // We need to pass the receiver to each of the workers. But we cannot
+        // have multiple receivers in a Multi-producer, Single-consumer channel!
+        // The solution is to wrap the receiver in Arc and Mutex so that we can
+        // safely share multiple references to the receiver across workers.
+        // TODO: Create reference counted mutex for receiver
+
         for id in 0..size {
+            // TODO: Uncomment me!
             // workers.push(Worker::new(id, receiver.clone()));
         }
 
@@ -37,12 +55,20 @@ impl ThreadPool {
         })
     }
 
+    /// Sends the job to a worker that will execute the job when available
     pub fn execute<F>(&self, f: F)
         where
             F: FnOnce() + Send + 'static
     {
-        let job = Box::new(f);
+        // We cannot send a value of unknown size unless we use `Box`
+        // Note: `Job` is a type alias for `Box<dyn FnOnce...>`
+        //       `Box<dyn T>` is a trait object which uses dynamic dispatch to
+        //       invoke our closure. The `Box` is a pointer that references
+        //       the code block we want to execute
+        let job: Job = Box::new(f);
+
         // TODO: Send job to channel
+        // Hint: Who owns the sender?
     }
 }
 
@@ -55,7 +81,10 @@ impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || {
             loop {
-                // TODO: Read jobs from channel and execute them
+                let job: Job;
+                // TODO: Read jobs from the channel and store in variable
+                // TODO: Uncomment me!
+                // job.execute();
             }
         });
 
@@ -66,12 +95,15 @@ impl Worker {
     }
 }
 
+// Hack for the compiler to properly take ownership of the value inside the Box
+// For more info on why this is needed, read the following:
+// https://doc.rust-lang.org/book/ch20-02-multithreaded.html#implementing-the-execute-method
 trait FnBox {
-    fn call_box(self: Box<Self>);
+    fn execute(self: Box<Self>);
 }
 
 impl<F: FnOnce()> FnBox for F {
-    fn call_box(self: Box<F>) {
+    fn execute(self: Box<F>) {
         (*self)()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
+use rust_workshop::ThreadPool;
 use std::fs;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::time::Duration;
 
+/// Creates a Web Server listening on port `7878` and passes connections to
+/// `handle_connection()`
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     println!("\n\nType 'http://127.0.0.1:7878/' into your browser!");
     println!("Type 'http://127.0.0.1:7878/slow' to simulate a slow request!");
 
+    // Creates a pool of 5 workers
+    // Note: try running with a ThreadPool of size 0 and see what happens!
+    let pool = ThreadPool::new(5).unwrap();
+
+    // Iterate over various socket connections
+    // Note: `TcpListener` will never generate a `None` value in the iterator
+    //       This results in the main thread looping forever.
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
@@ -17,13 +27,22 @@ fn main() {
     }
 }
 
+/// Read the browser request and send back the proper response
+/// Valid Requests:
+///     '/' => Hello response
+///     '/slow' => Slow hello response (simulates cpu-intensive page)
+/// Invalid Requests => 404 response
 fn handle_connection(mut stream: TcpStream) {
+    // Read browser request
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
 
+    // Hardcoded valid request headers
     let get = b"GET / HTTP/1.1\r\n";
     let slow = b"GET /slow HTTP/1.1\r\n";
 
+    // Naive parsing of GET request generates a tuple of response header
+    // and path to response contents file
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK\r\n\r\n", "assets/hello.html")
     } else if buffer.starts_with(slow) {
@@ -33,6 +52,7 @@ fn handle_connection(mut stream: TcpStream) {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "assets/404.html")
     };
 
+    // write reponse header and contents to stream for browser to display
     let contents = fs::read_to_string(filename).unwrap();
     let response = format!("{}{}", status_line, contents);
     stream.write(response.as_bytes()).unwrap();
