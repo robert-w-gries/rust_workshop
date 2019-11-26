1429a25e6af9be5539a9998201cb999fe79c63ed
Bharathi Seshadri <bseshadr@cisco.com>
2019-11-26 09:57:35 -0800

    fix borrow example

--- examples/borrow.rs
+++ examples/borrow.rs
@@ -1,16 +1,16 @@
 fn test_borrow() {
     let mut a = String::new();
     a.push_str("Hello");
-    let mut b = a;
-    println!("b = {}", b);
+    let mut b = a; // Move ownership of a to b
+    println!("b = {}",b);
     {
-        let c = &mut b;
-        println!("c = {}", c);
+        let  c = &mut b; // Take mutable reference of b
         c.push_str(" World!");
+        println!("c = {}",c); // Hello World!
     }
-    println!("b = {}", b);
+    println!("b = {}",b); // Hello World!
 }
 
 fn main() {
-    test_borrow();
+  test_borrow();
 }
